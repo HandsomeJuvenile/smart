43a761e3b62396af9c33db2111a4efa85f8a240b
HandsomeJuvenile <1414790478@qq.com>
2018-03-05 12:03:53 +0800

    1.0.0

--- src/main/java/com/ace/smart/annotation/service/RowNameService.java
+++ src/main/java/com/ace/smart/annotation/service/RowNameService.java
@@ -12,59 +12,29 @@ import java.util.List;
 
 public class RowNameService {
 
-    public static void load(Class<?> clz,List<?> list) throws Exception {
+    private Method rM;
+
+    /**
+     * 解析注解@RowName 为excel第一行设立值
+     * @param clz
+     * @return
+     * @throws Exception
+     */
+    public static List<String> load(Class<?> clz) throws Exception {
         Object object = clz.newInstance();
         Object str ;
         Annotation[] annotations = clz.getAnnotations();
-
         // 获得到类的属性注解
-        //Field[ ] fields = clz.getDeclaredFields();
+        Field[ ] fields = clz.getDeclaredFields();
         List<String> titles = new ArrayList();
-
-        for (Object obj: list) {
-            Field[ ] fields =  obj.getClass().getDeclaredFields();
-            for (Field field:fields) {
-                Field f = clz.getDeclaredField(field.getName());
-                RowName afd = f.getAnnotation(RowName.class);
-                if(afd !=null){
-                    PropertyDescriptor descriptor = new PropertyDescriptor(f.getName(),clz);
-                    Method rM = descriptor.getReadMethod();
-                    if(f.getType().equals(Integer.class)){
-                        str = (Integer) rM.invoke(object);
-                    }else {
-                        str = (String) rM.invoke(object);
-                    }
-                    System.out.println(afd.name()+"           "+str);
-                    titles.add(afd.name());
-                }
+        for (Field field:fields) {
+            Field f = clz.getDeclaredField(field.getName());
+            RowName afd = f.getAnnotation(RowName.class);
+            if(afd !=null){
+                titles.add(afd.name());
             }
         }
-
-
-
+        return titles;
     }
 
-
-
-
-
-    public static void main(String[] args) {
-        List<PUser> list = new ArrayList<PUser>();
-        PUser pUser = new PUser();
-        pUser.setEmail("1414790478@");
-        pUser.setPhone("136546");
-        pUser.setUserLoginName("zzh");
-        pUser.setPswd("1111111");
-        pUser.setStatus("0");
-        pUser.setCreateTime("2017-08-20");
-        pUser.setId(1654651465165466l);
-        pUser.setSelfIntroduction("HELLO");
-        pUser.setuAddress("wuhu");
-        list.add(pUser);
-        try {
-            load(PUser.class,list);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
--- src/main/java/com/ace/smart/controller/ExcelController.java
+++ src/main/java/com/ace/smart/controller/ExcelController.java
@@ -1,5 +1,6 @@
 package com.ace.smart.controller;
 
+import com.ace.smart.annotation.service.RowNameService;
 import com.ace.smart.entity.PUser;
 import com.ace.smart.excel.ExportExcel;
 import com.ace.smart.excel.entity.ExcelData;
@@ -31,24 +32,15 @@ public class ExcelController {
     public void excel(HttpServletResponse response) throws Exception {
         ExcelData data = new ExcelData();
         data.setName("用户");
-        List<String> titles = new ArrayList();
-        titles.add("用户名");
-        titles.add("真实姓名");
-        titles.add("年龄");
-        titles.add("住址");
-        titles.add("邮箱");
-        titles.add("手机号");
-        titles.add("创建时间");
-        titles.add("自我介绍");
-        data.setTitles(titles);
+        data.setTitles(RowNameService.load(PUser.class));
         List<PUser> pUsersList =  pUserService.selectAll(0,100).getList();
         List<List<Object>> rows = new ArrayList();
         for (PUser pUser:pUsersList) {
             List<Object> row = new ArrayList();
             row.add(pUser.getUserLoginName());
             row.add(pUser.getNickname());
-            row.add(pUser.getuAge());
-            row.add(pUser.getuAddress());
+            row.add(pUser.getUAge());
+            row.add(pUser.getUAddress());
             row.add(pUser.getEmail());
             row.add(pUser.getPhone());
             row.add(pUser.getCreateTime());
@@ -58,7 +50,9 @@ public class ExcelController {
         data.setRows(rows);
         String fileName = System.currentTimeMillis()+".xlsx";
         ExportExcel.exportExcel(response,fileName,data);
-        LOGGER.info("用户excel导出成功"+fileName);
+        if (LOGGER.isInfoEnabled()){
+            LOGGER.info("用户excel导出成功"+fileName);
+        }
     }
 
 }
