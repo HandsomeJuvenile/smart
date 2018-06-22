3ba4efaf1597c447a7e9e4b4bea9911f4c3ece83
HandsomeJuvenile <1414790478@qq.com>
2018-06-22 11:33:58 +0800

    1.0.0

--- src/main/java/com/ace/smart/job/EveryDayEmailJob.java
+++ src/main/java/com/ace/smart/job/EveryDayEmailJob.java
@@ -20,17 +20,8 @@ public class EveryDayEmailJob implements Job{
     @Autowired
     private SendEmail sendEmail;
     private static final String [] emalis =
-            {       "1837400570@qq.com","1270943107@qq.com","1208114563@qq.com",
-                    "1319759684@qq.com","595186151@qq.com","1344613837@qq.com",
-                    "18356090837@163.com","xiangrikuiforever@qq.com","1097337990@qq.com",
-                    "35474616@qq.com","2627021941@qq.com","1056350018@qq.com",
-                    "372129190@qq.com","791217653@qq.com","2444017726@qq.com",
-                    "2779902638@qq.com","1151857475@qq.com","1518678988@qq.com",
-                    "2592684466@qq.com","1322843232@qq.com","2752380184@qq.com",
-                    "772361744@qq.com","940003078@qq.com","1642518875@qq.com",
-                    "2586513592@qq.com"
+            {
             };
-    private String emaill = "1414790478@qq.com";
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
@@ -51,21 +42,5 @@ public class EveryDayEmailJob implements Job{
         logger.info("早安结束，新的一天开始！");
     }
 
-    public void emaill() throws JobExecutionException {
-        Email email = new Email();
-        StringBuilder imgPath = new StringBuilder("f:/image/email/");
-        File file = new File("f:/image/email/2018.06.11.jpg");
-        imgPath.append("2018.06.11.jpg");
-        email.setImgPath(imgPath.toString());
-        email.setSubject("早安");
-            try {
-                email.setRecive(emaill);
-                sendEmail.sendPicEmail(email);
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-    }
+
 }
